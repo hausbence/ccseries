9636cba1841af2c870735d825ce45fd65adb1dd8
hausbence <bence.hausknecht@gmail.com>
2020-04-27 12:28:05 +0200

    done

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,26 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_actors():
+    return data_manager.execute_select('''
+        SELECT actors.name, COUNT(show_characters.actor_id) as num_of_roles, actors.id
+        from actors
+        INNER JOIN show_characters ON actors.id = show_characters.actor_id
+        INNER JOIN shows ON show_characters.show_id = shows.id
+        WHERE death IS NOT NULL
+        GROUP BY actors.id
+        ORDER BY num_of_roles DESC
+        LIMIT 10
+    ''')
+
+
+def get_shows_by_actors(ids):
+    return data_manager.execute_select('''
+        SELECT show_characters.character_name, actors.name, shows.title
+        FROM show_characters
+        INNER JOIN actors ON show_characters.actor_id = actors.id
+        INNER JOIN shows ON show_characters.show_id = shows.id
+        WHERE show_characters.actor_id IN %(ids)s
+    ''', {'ids': ids})
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, url_for
 from data import queries
 
@@ -13,6 +15,15 @@ def index():
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+
+@app.route('/actors')
+def get_actors():
+    actor_names = queries.get_actors()
+    list_of_dict = json.loads(json.dumps(actor_names))
+    ids = tuple([x['id'] for x in list_of_dict])
+    actors = queries.get_shows_by_actors(ids)
+    return render_template('actors.html', actors = actors)
 
 
 def main():
